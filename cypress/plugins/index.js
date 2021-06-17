945a1ae4631ce1e77112db92e8c8c3fb89290336
Bala <balakrishnan.ammaiappan@camelotls.com>
2021-06-17 18:50:42 +0530

    Added the check  --  Only upload videos for specs with failing or retried tests

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,6 +12,11 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+// need to install these dependencies
+// npm i lodash del --save-dev
+const _ = require('lodash')
+const del = require('del')
+
 /**
  * @type {Cypress.PluginConfig}
  */
@@ -19,4 +24,16 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on('after:spec', (spec, results) => {
+    if (results && results.video) {
+      // Do we have failures for any retry attempts?
+      const failures = _.some(results.tests, (test) => {
+        return _.some(test.attempts, { state: 'failed' })
+      })
+      if (!failures) {
+        // delete the video if the spec passed and no tests retried
+        return del(results.video)
+      }
+    }
+  })
 }
