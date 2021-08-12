1c9475e30947717d51fb126d9d9536b7161a1f38
Bala <balakrishnan.ammaiappan@camelotls.com>
2021-08-12 16:08:22 +0530

    Added the A11Y check with include/ exclude/ disable rules check

--- cypress/integration/axe/elements/checkBox.spec.js
+++ cypress/integration/axe/elements/checkBox.spec.js
@@ -6,11 +6,20 @@ import CheckBox from '../../../helper/pages/elements/CheckBox';
 import Menu from '../../../helper/static/MenuEnum';
 import State from '../../../helper/static/StateEnum';
 import GenericFun from '../../../helper/utils/GenericFun';
+import { injectAxe } from 'cypress-axe';
 
 const genericFun = new GenericFun();
 const home = new Home();
 const leftPanel = new LeftPanel();
 const checkBox = new CheckBox();
+
+/**
+ * This case will help us to understand the 
+ *    exclude specific elements
+ *    include specific elements
+ *    disable axe rules
+ * for the A11Y violations
+ */
 
 describe(`${COMPONENT.checkBox}`, () => {
 
@@ -29,10 +38,25 @@ describe(`${COMPONENT.checkBox}`, () => {
     })
 
     it('should not contains any accessiblity violations', () => {
-      cy.injectAxe()
-      cy.checkA11y()
+      cy.injectAxe();
+      cy.checkA11y();
     });
 
+
+    it('should exlcude the specific elements on the page for a11y violations', () => {
+      cy.injectAxe();
+      cy.checkA11y({ exclude: ['.element-list > .menu-list']});
+    });
+
+    it('should test only the specific elements on the page for a11y violations', () => {
+      cy.injectAxe();
+      cy.checkA11y('.element-list > .menu-list');
+    });
+
+    it('should only include rules with serious and critical impacts', () => {
+      cy.injectAxe();
+      cy.checkA11y(null, {includedImpacts:['critical','serious']});
+    });
   })
 
 });
